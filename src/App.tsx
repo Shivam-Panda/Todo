723901c290cbf3f625a5561d570a2cf9e486b7fd
Shivam Panda <panda.shivam81@bloomfield.org>
2022-06-30 12:02:46 -0400

    List Working

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+const App: React.FC = () => {
+  const [list, setList] = useState<Array<string>>([]);
+  const [word, setWord] = useState<string>('')
+
+  useEffect(() => {
+    console.log(word);
+  }, [word])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {list.map((val, ind) => {
+        return (
+          <div>
+            <p>{val}</p>
+            <button onClick={() => {
+              let l = []
+              for(let i = 0; i < list.length; i++) {
+                if(i == ind) {
+                  // Do Nothing
+                } else {
+                  l.push(list[i]);
+                }
+                setList(l);
+              }
+            }}>Delete</button>
+          </div>
+        )
+      })}
+      <input value={word} onKeyDown={(e) => {
+        if(e.key == 'Enter') {
+          let cur = list;
+          cur.push(word);
+          setWord('')
+        } else if(e.key == 'Backspace') {
+          let cur = '';
+          for(let i = 0; i < word.length - 1; i++) {
+            cur += word[i];
+          }
+          setWord(cur);
+        } else if(e.key.length == 1) {
+          let cur = word;
+          cur += e.key;
+          setWord(cur);
+        }
+      }} />
+      <button
+      onClick={() => {
+        let cur = list;
+        cur.push(word);
+        setWord('')
+      }}>Set</button>
+      <button onClick={() => {
+        setList([])
+        setWord('');
+      }}>Reset</button>
     </div>
-  );
-}
+  )
+};
 
 export default App;
